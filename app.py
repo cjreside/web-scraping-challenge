275ad491db15edbfab1133ee5d6dbd3ec7400877
cjreside <connorjreside@gmail.com>
2020-08-29 09:53:06 -0400

    update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/app")
 
 # create route that renders index.html template
 @app.route("/")
-def echo():
+def home():
      # Find data
     mars_dict = mongo.db.collection.find_one()
     #Return templated data
@@ -22,10 +22,10 @@ def echo():
 @app.route("/scrape")
 def scrape():
     # Run the scrape function
-    mars_dict = scrape_mars.scrape()
+    mars_dict_scrape = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_dict, upsert=True)
+    mongo.db.collection.update({}, mars_dict_scrape, upsert=True)
 
     # Redirect back to home page
     return redirect("/", code=302)
